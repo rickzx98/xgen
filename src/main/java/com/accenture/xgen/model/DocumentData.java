0b4e2697388a9b9099621f4615bcb46580de4cd5
philipensomo <philipensomo@gmail.com>
2018-02-01 08:11:59 -0500

    added param for version, removed ROW_COUNT footer

--- src/main/java/com/accenture/xgen/model/DocumentData.java
+++
@@ -1,6 +0,0 @@
-package com.accenture.xgen.model;
-
-public interface DocumentData {
-    public String construct(Structure structure, StructureData structureData) throws Structure.StructureException;
-    public long recordNumber();
-}
